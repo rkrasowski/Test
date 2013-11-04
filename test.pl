c3e6e7e490bc059c60c8b88b1f655630ea964f3e
rkrasowski <rkrasowski@yahoo.com>
2013-11-04 18:46:51 -0500

    initial project version

---
+++ test.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $test = "variable";
+my $debug = 1;
+
+debug("hello from debug $test");
+
+
+sub debug 
+	{	
+		my $sentence = shift;
+		if ($debug == 1)
+			{
+				print "$sentence\n";
+			}
+	}
+
+
+
