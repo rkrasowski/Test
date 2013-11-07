d03f42759e731853b118a26aae625eb52e1b0cc9
rkrasowski <rkrasowski@yahoo.com>
2013-11-06 19:46:25 -0500

    Branch commit

---
+++ test.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $test = "variable";
+my $debug = 1;
+
+debug("hello from debug $test");
+
+
+sub debug 
+	{	
+		my $sentence = shift;
+		if ($debug == 1)
+			{
+				print "$sentence\n";
+			}
+	}
+
+print "I have just added line to it\n";
+print "next line\n";
+print "trzecia linia\n";
+print "Test branch\n";
